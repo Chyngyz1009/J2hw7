8d3e23baba9a01ab86dcdf5775edc54f413e7c9d
Chyngyz1009 <chika_0410@inbox.ru>
2020-11-10 15:58:28 +0600

    peredelal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,80 +8,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String a1 = scanner.nextLine();
-        String a2 = scanner.nextLine();
-        String a3 = scanner.nextLine();
-        String a4 = scanner.nextLine();
-        String a5 = scanner.nextLine();
-
-        ArrayList<Object> a = new ArrayList<>();
-        a.add(a1);
-        a.add(a2);
-        a.add(a3);
-        a.add(a4);
-        a.add(a5);
-
-        Iterator<Object> iter = a.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-
-        }
+        ArrayList<String> a = new ArrayList<>();
+        addString(a, "input text");
         System.out.println("--------");
 
-        String b1 = scanner.nextLine();
-        String b2 = scanner.nextLine();
-        String b3 = scanner.nextLine();
-        String b4 = scanner.nextLine();
-        String b5 = scanner.nextLine();
-
-        ArrayList<Object> b = new ArrayList<>();
-        b.add(b1);
-        b.add(b2);
-        b.add(b3);
-        b.add(b4);
-        b.add(b5);
-        Iterator<Object> iter1 = b.iterator();
-
-        while (iter1.hasNext()) {
-            System.out.println(iter1.next());
-        }
+        ArrayList<String> b = new ArrayList<>();
+        addString(b, "input text");
         System.out.println("-------");
 
-
         List<String> c = new ArrayList<>();
-        c.add(a1);
-        c.add(b5);
-        c.add(a2);
-        c.add(b4);
-        c.add(a3);
-        c.add(b3);
-        c.add(a4);
-        c.add(b2);
-        c.add(a5);
-        c.add(b1);
-        Iterator<String> iter2 = c.iterator();
-        while (iter2.hasNext()) {
-            System.out.println(iter2.next());
+        ListIterator<String> listIterator = b.listIterator();
+        ListIterator<String> strIterator = a.listIterator();
+
+        while (strIterator.hasNext()) {
+            String ss1 = strIterator.next();
+            c.add(ss1);
+            if (listIterator.hasNext()) {
+                String ss2 = listIterator.next();
+                c.add(ss2);
+            }
         }
-        System.out.println("--------");
+        System.out.println(c);
+
         c.sort(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return o1.length() - o2.length();
             }
         });
+
         System.out.println("----------");
-        System.out.println("sortirvka");
-        Iterator<String> iter3 = c.iterator();
-        while (iter3.hasNext()) {
-            System.out.println(iter3.next());
-        }
-
-
+        System.out.println("sortirovka");
+        System.out.println(c);
     }
 
-
+    public static void addString(ArrayList<String> list, String word) {
+        while (list.size() < 5) {
+            System.out.println(word);
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            list.add(s);
+        }
+        System.out.println(list);
+    }
 }
 
 
